2bfa31011d8238cd02587cfe21a6499429e20050
Maria-7514 <evangeline73.mjhe@gmail.com>
2021-03-08 19:45:03 +0000

    excuse

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = [" my homework", " the keys", " the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+let elementa = Math.floor(Math.random() * who.length);
+let elementb = Math.floor(Math.random() * action.length);
+let elementc = Math.floor(Math.random() * what.length);
+let elementd = Math.floor(Math.random() * when.length);
+
+let cambioa = who[elementa];
+let cambiob = action[elementb];
+let cambioc = what[elementc];
+let cambiod = when[elementd];
+
+let change = document.getElementById("excuse");
+change.innerHTML = cambioa + " " + cambiob + cambioc + " " + cambiod;
